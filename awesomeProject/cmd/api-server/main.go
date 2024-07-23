bd151670536614f06e34c68c21f8855f440cd212
IanHanna <Ian.hanna@mail.schwarz>
2024-07-23 13:46:24 +0200

    refactored project to fit REST standard

--- awesomeProject/cmd/api-server/main.go
+++ awesomeProject/cmd/api-server/main.go
@@ -21,8 +21,8 @@ func main() {
 	router.HandleFunc("/items/{id}", handlers.DeleteItemByIDHandler).Methods("DELETE")
 	router.HandleFunc("/items", handlers.DeleteAllItemsHandler).Methods("DELETE")
 
-	log.Println("Starting server on :8080")
-	if err := http.ListenAndServe(":8080", router); err != nil {
+	log.Println("Starting server on :8081")
+	if err := http.ListenAndServe(":8081", router); err != nil {
 		log.Fatalf("Error starting server: %v", err)
 	}
 }
