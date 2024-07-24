20b7efbdc0ac0486ab7d0559f9467cf15b21938f
IanHanna <Ian.hanna@mail.schwarz>
2024-07-24 08:32:56 +0200

    refactored user.go

--- awesomeProject/internal/permissions/user/user.go
+++ awesomeProject/internal/permissions/user/user.go
@@ -1,16 +1,19 @@
 package user
 
-import "github.com/google/uuid"
+import (
+	"github.com/google/uuid"
+	"strings"
+)
 
 type User struct {
 	ID           uuid.UUID `json:"id,omitempty" gorm:"type:char(36);primaryKey"`
 	Username     string    `json:"username" gorm:"column:username"`
 	Identifier   string    `json:"identifier" gorm:"column:identifier"`
 	IdentifierID uuid.UUID `json:"identifier_id" gorm:"column:identifier_id"`
-	Permissions  View
+	Permissions  PermissionView
 }
 
-type View interface {
+type PermissionView interface {
 	CanView(isAdminContent bool) bool
 	CanCreate() bool
 	CanUpdate() bool
@@ -18,15 +21,16 @@ type View interface {
 	CanViewAll() bool
 }
 
-type AdminView struct {
-}
+// AdminView with full permissions
+type AdminView struct{}
 
-func (v AdminView) CanView(isAdminContent bool) bool { return true }
-func (v AdminView) CanCreate() bool                  { return true }
-func (v AdminView) CanUpdate() bool                  { return true }
-func (v AdminView) CanDelete() bool                  { return true }
-func (v AdminView) CanViewAll() bool                 { return true }
+func (v AdminView) CanView(bool) bool { return true }
+func (v AdminView) CanCreate() bool   { return true }
+func (v AdminView) CanUpdate() bool   { return true }
+func (v AdminView) CanDelete() bool   { return true }
+func (v AdminView) CanViewAll() bool  { return true }
 
+// UserView  with limited permissions
 type UserView struct{}
 
 func (v UserView) CanView(isAdminContent bool) bool { return !isAdminContent }
@@ -35,6 +39,7 @@ func (v UserView) CanUpdate() bool                  { return false }
 func (v UserView) CanDelete() bool                  { return false }
 func (v UserView) CanViewAll() bool                 { return false }
 
+// DefaultView  no permissions
 type DefaultView struct{}
 
 func (v DefaultView) CanView(isAdminContent bool) bool { return false }
@@ -43,8 +48,8 @@ func (v DefaultView) CanUpdate() bool                  { return false }
 func (v DefaultView) CanDelete() bool                  { return false }
 func (v DefaultView) CanViewAll() bool                 { return false }
 
-func GetView(identifier string) View {
-	switch identifier {
+func GetView(identifier string) PermissionView {
+	switch strings.ToLower(identifier) {
 	case "admin":
 		return AdminView{}
 	case "user":
